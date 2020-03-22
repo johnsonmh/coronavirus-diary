5ddc8ecdfacac7491922d535451a4b3e43f0c77e
MH Johnson <johnsonmh@google.com>
2020-03-22 17:53:09 -0400

    Add setUp so that widget tests work with HydratedBloc

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,19 @@
+import 'dart:io';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:covidnearme/src/app.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 void main() {
+  setUp(() async {
+    final directory = await Directory.systemTemp.createTemp();
+    BlocSupervisor.delegate = await HydratedBlocDelegate.build(
+      storageDirectory: directory,
+    );
+  });
+
   testWidgets('App loads test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(App());
